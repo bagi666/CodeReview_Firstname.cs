5ed893449474e9dd25185e5108a6d5ae649f7e05
bagi666 <yakushev2012b@yandex.ru>
2017-03-09 08:23:29 +0600

    Update Analysis_Firstname.cs
    Логика обоих методов класса Analysis одинаковая, за исключением того, что:
    1) в Первом методе, при случайно сгенерированном числе меньшим, чем 0,749999999999999 - конечное возвращаемое методом значение всегда будет <= 0, в противном случае равно 1.
    2) А во Втором методе, при случайно сгенерированном числе меньшим, чем примерно 0,667224079 - конечное возвращаемое методом значение всегда будет <= 0, в противном случае равно 1.
    Таким образом, при одинаковых сгенерированных числах у обоих методов, количество возвращаемых значений <=  Нуля будет больше у 1-го метода, чем у 2-го.
    Если что-то другое, то прошу прокомментировать Ваш ответ!

--- Analysis_Firstname.cs
+++ Analysis_Firstname.cs
@@ -6,30 +6,43 @@ namespace Interview
 
 {
 
-public class Analysis
+	public class Analysis
 
-{
+	{
 
-public double GetRandomNumber1()
+		public double GetRandomNumber1()
 
-{
+		{
 
-Random random = new Random();
+			Random random = new Random();
+			// Round() - Округляет значение до ближайшего целого или указанного количества десятичных знаков.			
+			// Random.NextDouble() - Возвращает случайное число с плавающей запятой, которое больше или равно 0,0 и меньше 1,0.
+			return Round((random.NextDouble() - 0.5) * 2);
 
-return Round((random.NextDouble() - 0.5) * 2);
+		}
+
+		public double GetRandomNumber2()
+
+		{
+
+			Random random = new Random();
+
+			return Round((random.NextDouble() - 0.5) * 2.99);
+
+		}
+
+	}
 
 }
 
-public double GetRandomNumber2()
+class MainAnalysis {
 
-{
+	public static void Main() {
 
-Random random = new Random();
-
-return Round((random.NextDouble() - 0.5) * 2.99);
-
-}
-
-}
+		Interview.Analysis A = new Interview.Analysis();
+		Console.WriteLine("{0}", A.GetRandomNumber1());
+		Console.WriteLine("\n{0}", A.GetRandomNumber2());
+		
+	}
 
 }
